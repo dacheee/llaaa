4409ad36618033e00936e88e6c129c3c1702f25a
Dacheee <dace.kalisuka@gmail.com>
2021-07-09 21:41:21 +0300

    first commit

---
+++ src/math.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class math {
+    public static void main(String[] args) {
+
+
+//        double varA, varB, varC, varD, varE, varP;
+//        int varX;
+//        varP = 3.14;
+//        varA = 5.5;
+//        varB = 8.5;
+//        varC = 5012;
+//        varD = 623;
+//        varE = 2;
+//        varX = 25;
+//
+
+//        System.out.println(Math.round(varP*Math.pow(varA,2)));
+//        System.out.println(2*varP*varA);
+//        System.out.println((varB+varC+varD+varE)/4);
+//        System.out.println(varA*varB);
+//        System.out.println(2*(varA+varB));
+//        System.out.println();
+//        Scanner  scanner = new Scanner(System.in);
+//        int intTosum = scanner.nextInt();
+
+//        lesson 6
+//        age should be positive
+//        Scanner scanner = new Scanner(System.in);
+//        int age = scanner.nextInt();
+//        if (age >= 18) {
+//            System.out.println("enjoy the boozzzze");
+//        }
+//        if (age < 18) {
+//            System.out.println("no no noooo");
+//
+
+
+
+    }
+}
+
